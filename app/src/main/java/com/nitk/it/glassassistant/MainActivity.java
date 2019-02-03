c4ddb70b788c87d23b4ef4ebbf5746accbb01698
hafeezali <hafeez.ali5@gmail.com>
2019-02-03 21:43:53 +0530

    Added tap triggered menu and gestureDetector

--- app/src/main/java/com/nitk/it/glassassistant/MainActivity.java
+++ app/src/main/java/com/nitk/it/glassassistant/MainActivity.java
@@ -2,17 +2,18 @@ package com.nitk.it.glassassistant;
 
 import android.app.Activity;
 import android.content.Context;
-import android.media.AudioManager;
 import android.os.Bundle;
-import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 
-import com.google.android.glass.media.Sounds;
+import com.google.android.glass.touchpad.Gesture;
+import com.google.android.glass.touchpad.GestureDetector;
 import com.google.android.glass.view.WindowUtils;
 import com.google.android.glass.widget.CardBuilder;
 import com.google.android.glass.widget.CardScrollAdapter;
@@ -45,6 +46,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         getWindow().requestFeature(WindowUtils.FEATURE_VOICE_COMMANDS);
 
         mView = buildView();
@@ -74,16 +76,67 @@ public class MainActivity extends Activity {
                 return AdapterView.INVALID_POSITION;
             }
         });
+
         // Handle the TAP event.
         mCardScroller.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Plays disallowed sound to indicate that TAP actions are not supported.
-                AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-                am.playSoundEffect(Sounds.DISALLOWED);
+                openOptionsMenu();
             }
         });
+
+        mGestureDetector = createGestureDetector(this);
         setContentView(mCardScroller);
+    }
+
+    private GestureDetector createGestureDetector(Context context) {
+        GestureDetector gestureDetector = new GestureDetector(context);
+
+        gestureDetector.setBaseListener(new GestureDetector.BaseListener() {
+            @Override
+            public boolean onGesture(Gesture gesture) {
+                if (gesture == Gesture.TAP) {
+                    openOptionsMenu();
+                    return true;
+                } else if (gesture == Gesture.TWO_TAP) {
+                    // do something on two finger tap
+                    return true;
+                } else if (gesture == Gesture.SWIPE_RIGHT) {
+                    // do something on right (forward) swipe
+                    return true;
+                } else if (gesture == Gesture.SWIPE_LEFT) {
+                    // do something on left (backwards) swipe
+                    return true;
+                } else if (gesture == Gesture.SWIPE_DOWN) {
+                    finish();
+                }
+                return false;
+            }
+        });
+
+        gestureDetector.setFingerListener(new GestureDetector.FingerListener() {
+            @Override
+            public void onFingerCountChanged(int i, int i1) {
+
+            }
+        });
+
+        gestureDetector.setScrollListener(new GestureDetector.ScrollListener() {
+            @Override
+            public boolean onScroll(float v, float v1, float v2) {
+                return false;
+            }
+        });
+
+        return gestureDetector;
+    }
+
+    @Override
+    public boolean onGenericMotionEvent(MotionEvent event) {
+        if (mGestureDetector != null) {
+            return mGestureDetector.onMotionEvent(event);
+        }
+        return false;
     }
 
     @Override
@@ -100,15 +153,21 @@ public class MainActivity extends Activity {
         if (featureId == WindowUtils.FEATURE_VOICE_COMMANDS || featureId == Window.FEATURE_OPTIONS_PANEL) {
             switch (item.getItemId()) {
                 case R.id.connect_socket:
+                    connect("Sockets");
                     break;
                 case R.id.connect_bluetooth:
+                    connect("Bluetooth");
                     break;
                 case R.id.connect_raspPi:
+                    connect("RaspberryPi");
                     break;
             }
             return true;
         }
         return super.onMenuItemSelected(featureId, item);
+    }
+
+    private void connect(String platform) {
     }
 
     @Override
@@ -124,7 +183,7 @@ public class MainActivity extends Activity {
     }
 
     /**
-     * Builds a Glass styled "Hello World!" view using the {@link CardBuilder} class.
+     * Builds a Glass styled "Describe Scene!" view using the {@link CardBuilder} class.
      */
     private View buildView() {
         CardBuilder card = new CardBuilder(this, CardBuilder.Layout.COLUMNS);
