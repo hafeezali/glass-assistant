42a643ecbd22a9508c0eb19f0d0095098db5f1c3
hafeezali <hafeez.ali5@gmail.com>
2019-02-06 14:27:12 +0530

    Added Server in java

--- Server/src/CaptionServer.java
+++ Server/src/CaptionServer.java
@@ -1,8 +1,9 @@
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,7 +12,9 @@ public class CaptionServer {
     private int port;
     private Socket socket = null;
     private ServerSocket serverSocket = null;
-    private ObjectInputStream objectInputStream = null;
+    private InputStream inputStream = null;
+    private byte[] imageBytes;
+    private ByteArrayInputStream byteArrayInputStream;
     private BufferedImage bufferedImage = null;
     private Integer count = 0;
     private String path = "/home/hafeez/Desktop/Major Project/ImagesRecieved/";
@@ -21,9 +24,11 @@ public class CaptionServer {
     private CaptionServer(int port) {
         this.port = port;
         try {
+            System.out.println("Hello");
             serverSocket = new ServerSocket(port);
             socket = serverSocket.accept();
-            objectInputStream = new ObjectInputStream(socket.getInputStream());
+            System.out.println("Connection Established");
+            inputStream = socket.getInputStream();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -37,12 +42,13 @@ public class CaptionServer {
     private void listen() {
         while (true) {
             try {
-                bufferedImage = (BufferedImage) this.objectInputStream.readObject();
+                System.out.println("Waiting for image");
+                inputStream.read(imageBytes);
+                byteArrayInputStream = new ByteArrayInputStream(imageBytes);
+                bufferedImage = ImageIO.read(byteArrayInputStream);
                 ImageIO.write(bufferedImage, "jpg", new File(path + count.toString() + "." + imageType));
                 caption = getCaption();
             } catch (IOException e) {
-                e.printStackTrace();
-            } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
         }
